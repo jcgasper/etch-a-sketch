3d4cc96d1c5f643f3f9eb6e25ee49275ffcc5803
jcgasper <jacobgasper@gmail.com>
2022-12-01 16:01:24 -0500

    basic UI outline

---
+++ script.js
@@ -0,0 +1,9 @@
+const gridContainer = document.querySelector(".gridContainer");
+
+
+function generateGrid(size) {
+    let box = document.createElement("div");
+    console.log("generate func test");
+    gridContainer.append(box);
+}
+
