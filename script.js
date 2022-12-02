6fe67b804c8991042cfa5c3865e3c9a5c3c11c17
jcgasper <jacobgasper@gmail.com>
2022-12-02 14:45:29 -0500

    basic functionality, can generate 64x64 grid, NEED: hover/click func

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ const gridContainer = document.querySelector(".gridContainer");
 
 
 function generateGrid(size) {
+        //set up grid dimensions 
+        if (size > 100) {
+            size = 100;
+        }
+        gridContainer.style.gridTemplateColumns = `repeat(${size},1fr)`
+        gridContainer.style.gridTemplateRows = `repeat(${size},1fr)`
+
+
+        for (let i = 0; i < size*size; i++) {
     let box = document.createElement("div");
     console.log("generate func test");
+    box.classList.add("genBox");
     gridContainer.append(box);
+        }
+
 }
 
+generateGrid(64);
